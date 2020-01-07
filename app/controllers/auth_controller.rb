c6e8162f19121545b704adcf9df14f4ca2fd13bb
SimplyAvi <a.totaram@gmail.com>
2020-01-06 23:25:44 -0500

    working on user authenticate login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,16 @@
 class AuthController < ApplicationController
 
     def create
-        render json: 'hi'
+        # byebug
+        user = User.find_by(username: params[:username])
+        is_authenticated = user.authenticate(params[:password])
+       
+        if is_authenticated
+            render json: user
+        else 
+            render json: {errors: ["wrong username or password"]}, status: 422
+        end
+
     end
 
 end
